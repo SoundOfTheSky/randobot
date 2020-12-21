53553ed2ac87d5223e154ef0a36ee0ed3999dd48
Tkachev Nikita Stanislavovich <tkachiov.2000@gmail.com>
2020-12-21 16:30:29 +0300

    Fix then bot cant access message on reaction, and language command now on top

--- src/events/mr.js
+++ src/events/mr.js
@@ -4,9 +4,11 @@ module.exports = {
   title: 'Message reaction',
   event: 'message',
   handler: msg => {
-    if (msg.author.bot || msg.channel.type === 'dm' || Math.random() > 0.1) return;
-    const reactions = Utils.client.translations[Utils.client.guildsSettings[msg.guild.id].l]['message reactions'];
-    for (const l of reactions[Math.floor(Math.random() * reactions.length)].split(''))
-      msg.react(Utils.emojis[`:regional_indicator_${l}:`]);
+    try {
+      if (msg.author.bot || msg.channel.type === 'dm' || Math.random() > 0.1) return;
+      const reactions = Utils.client.translations[Utils.client.guildsSettings[msg.guild.id].l]['message reactions'];
+      for (const l of reactions[Math.floor(Math.random() * reactions.length)].split(''))
+        msg.react(Utils.emojis[`:regional_indicator_${l}:`]);
+    } catch {}
   },
 };
